db4557c134fee57a9ed41df1fbbc7504f8fa2e76
BestfoodIT <bestfoodit@hotmail.com>
2015-06-11 14:23:44 -0500

    6/11

---
+++ theme advice for Wordpress.cpp
@@ -0,0 +1,17 @@
+Theme advice for WordPress
+
+atr: 2   information 5   Okay for Modify "I am one" : Simple, but text heavy. 
+
+atr: 3.5 information 3   Okay for Modify "corpbiz": simple, but image heavy.
+
+atr: 4   information 3.5 Okay for Modify "vantage": well balance, clean.
+
+atr: 4.5 information 4   Okay for Modify "evolve": really complex, but can contain lots information(text and image);   "brother template of BFS"
+
+atr: 4   information 5	 Okay for Modify "AccessPress Parallax": Professional, contain load of information, highly recommand template for big company. "brother template of BFS"
+
+atr: 2.5 information 5   Okay for Modify "Minamaze":  rectangle information box, little bid edgy, text heavy.  
+
+atr: 4   information 4   Okay for Modify"onetone": Video slide show, may be over kill, but its a good theme for people to use. "brother template of BFS"
+
+atr: 5   information 5   Okay for Modify "Klean": Really really cool image base website!!!!  
